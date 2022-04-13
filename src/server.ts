0317bee69ac1f9a4ff2ae5671dd1fed3af2b762f
IsaacNogueira20 <isaacnogueira59057@gmail.com>
2022-04-13 20:59:10 -0300

    TRABALHO POO

---
+++ src/server.ts
@@ -0,0 +1,34 @@
+// function sum(num1:number,num2:number) {
+//     let n1 = Number(num1)
+//     let Soma = 0 -n1;
+//     if(num1 < 0){
+//         console.log('Os numeros não podem ser negativos, tente novamente !')
+//     }else if(num2 < 0){
+//         console.log('os numeros não podem ser negativos tente novamente')
+//     }else{
+//         for (let i = n1; i< num2; i++){
+//             Soma += i
+//         }
+//         console.log(Soma);
+//     }
+// }
+// sum(12, 15);
+
+// class pessoas{
+//     idade: number;
+//     nome: string;
+
+//     constructor(idade:number,nome:string){
+//         this.idade = idade
+//         this.nome = nome
+//     }
+// idade_meses(){
+//     return this.idade * 12
+//     }
+
+// } 
+
+// let t1 = new pessoas(19, "isaac nogueira");
+// console.log('a idade:  ',t1.idade);
+// console.log('nome: ', t1.nome);
+// console.log('idade em meses: ', t1.idade_meses());
